760115242a71b9d1a30dd151f360f7373f273b00
userOfkong <1045418792@qq.com>
2020-06-27 17:52:25 +0800

    首页完善

--- src/router.js
+++ src/router.js
@@ -19,22 +19,23 @@ export default new Router({
             name:'Home',
             component:Home,
             hidden: true
-        },{
-            path:'/home',
-            name:'导航一',
-            component:Home,
-            children:[
-                {
-                    path:'/test1',
-                    name:'选项1',
-                    component:Test1
-                },{
-                    path:'/test2',
-                    name:'选项2',
-                    component:Test2
-                }
-            ]
         }
+        // ,{
+        //     path:'/home',
+        //     name:'导航一',
+        //     component:Home,
+        //     children:[
+        //         {
+        //             path:'/test1',
+        //             name:'选项1',
+        //             component:Test1
+        //         },{
+        //             path:'/test2',
+        //             name:'选项2',
+        //             component:Test2
+        //         }
+        //     ]
+        // }
         // {
         //     path: '/',
         //     name: 'home',
